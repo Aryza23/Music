e39d7c9de8ab9c276efc88bcc8cee533d14ca5ec
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-02 18:31:37 +0700

    Update administrator.py

--- Music/MusicUtilities/helpers/administrator.py
+++ Music/MusicUtilities/helpers/administrator.py
@@ -3,7 +3,7 @@ from functools import wraps
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from pyrogram.types import Message
 from Music import app, SUDOERS
-from Music.Plugins.admins import member_permissions
+from Music.plugins.admins import member_permissions
 
 
 async def authorised(message):
