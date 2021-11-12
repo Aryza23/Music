b90be5fc1fd0313d71c6f5d3fabb9a5bbb2de4ed
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-12 18:21:07 +0700

    Update play.py

--- Music/plugins/play.py
+++ Music/plugins/play.py
@@ -248,7 +248,7 @@ async def play(_, message: Message):
         await LOG_CHAT(message, what)
         query = message.text.split(None, 1)[1]
         mystic = await message.reply_text("Processing Url")
-        ydl_opts = {"format": "bestaudio"}
+        ydl_opts = {"format": "bestaudio[extm4a]"}
         try:
             results = VideosSearch(query, limit=1)
             for result in results.result()["result"]:
